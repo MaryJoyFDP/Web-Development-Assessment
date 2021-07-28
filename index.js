328f0d4db49f864f0d66774b4b23b036c233e6ee
MaryJoyFDP <88038547+MaryJoyFDP@users.noreply.github.com>
2021-07-28 22:39:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+}
+else {
+    ready()
+} const cartData = [];
+// funtion for add to cart 
+function ready() {
+    const addToCartButtons = document.getElementsByClassName('button-add');
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i];
+        button.addEventListener('click', addToCartClicked);
+    }
+    const purchaseButton = document.getElementsByClassName('button-purchase');
+    var pBtn = purchaseButton[0].addEventListener('click', purchaseButtonClicked);
+}
+// Modal for Thank you
+function purchaseButtonClicked() {
+    alert('Thank you so much for purchasing!')
+    var cartItems = document.getElementsByClassName('cart-list')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    document.querySelector('.modal').style.display = "none";
+}
+// Button for remove
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.remove()
+}
+// Button for Add the details 
+function addToCartClicked(event) {
+    var button = event.target
+    var productItem = button.parentElement;
+    var itemName = productItem.getElementsByClassName('product-name')[0].innerText
+    var itemPrice = productItem.getElementsByClassName('product-price')[0].innerText
+    var imageSrc = productItem.getElementsByClassName('product-image')[0].src
+    itemName = itemName.replace(itemPrice, '');
+    cartData.push({ "name": itemName, "img": imageSrc, "price": itemPrice });
+    document.getElementById("cart-list").innerHTML = ` 
+    ${cartData.map(cartTemplate).join("")} `;
+    var removeCartItemButtons = document.getElementsByClassName('button-remove')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+function cartTemplate(prod) {
+    return ` 
+    <div class="cart-item"> 
+        <img class="cart-image" src="${prod.img}"> 
+        <h2 class="cart-name">${prod.name}</h2> 
+        <h1 class="cart-price">${prod.price}</h1> 
+        <button class="btn button-remove black" type='button'>Remove</button> </div> `;
+}
+// 
+function purchaseClicked() {
+    alert("Thank you so much for purchasing!");
+    var cartItems = document.getElementsById('cart-list');
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+}
